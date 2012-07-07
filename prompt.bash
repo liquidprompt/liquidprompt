96d0b6d19587cd75e457f96a01f669049a2cc671
nojhan <nojhan@gmail.com>
2012-07-07 11:35:58 +0200

    +license AGPLv3

--- prompt.bash
+++ prompt.bash
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# An intelligent an non intrusive prompt for bash
+
+# Licensed under the AGPL version 3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# 2012 (c) nojhan <nojhan@gmail.com>
+
+
 # Below are function for having a flexible dynamic prompt for power user:
 # - display the colored hostname when connected via ssh
 # - when root, color in yellow the [username path] and in red the sharp

05defce9d225a796048c89ab763a596dc459f4bd
nojhan <nojhan@gmail.com>
2012-08-17 16:31:45 +0200

    liquidize the time feature

--- liquid.ps1
+++ liquid.ps1
@@ -17,12 +17,13 @@
 # LP_SVN subversion
 # LP_ERR last error code
 # LP_MARK prompt mark
+# LP_TIME current time
 
 # Remember that most features come with their corresponding colors,
 # see the README.
 
 # add jobs, load and battery
-LP_PS1="${LP_BATT}${LP_LOAD}${LP_JOBS}"
+LP_PS1="${LP_TIME}${LP_BATT}${LP_LOAD}${LP_JOBS}"
 # add user, host and permissions colon
 LP_PS1="${LP_PS1}[${LP_USER}${LP_HOST}${LP_PERM}"
 

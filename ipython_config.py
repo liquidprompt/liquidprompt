013a38b84b11a62d431ff445a696089e932b5fce
Johann Dreo <johann.dreo@thalesgroup.com>
2013-01-17 17:37:53 +0100

    Add _lp_user to ipython

--- ipython_config.py
+++ ipython_config.py
@@ -1,6 +1,6 @@
 
 c = get_config()
-c.PromptManager.in_template = "{color.normal}[:{color.White}\Y0{color.normal}]\n\#{LP_MARK}"
+c.PromptManager.in_template = "{color.normal}[{LP_USER}:{color.White}\Y0{color.normal}]\n\#{LP_MARK}"
 c.PromptManager.justify = True
 c.TerminalInteractiveShell.confirm_exit = False
 
--- liquidprompt_ipython.py
+++ liquidprompt_ipython.py
@@ -1,13 +1,17 @@
+import os
+import getpass
 
 # TODO: load the liquidpromptrc config?
 
 class _lp_(object):
+
+
     styles = {"normal":0, "bold":1, "faint":2, "italic":3, "underline":4, "blink":5, "rapid_blink":6,
     "reverse":7, "conceal":8 }
     colors_mode8 = {"black":0, "red":1, "green":2, "yellow":3, "blue":4, "magenta":5, "cyan":6, "white":7}
     modes = {8:";", 256:";38;5;"}
 
-    def color( self, text, color, style ):
+    def color( self, text, color, style="normal" ):
         """Return the given text, surrounded by the given color ANSI markers."""
         # Special characters.
         start = "\033["
@@ -16,15 +20,20 @@ class _lp_(object):
         # Convert the color code.
         cs = str(self.styles[style])
 
-        # 8 colors modes
-        if color in self.colors_mode8:
+        # "normal" is a special keyword for no color
+        if color == "normal":
             mode = 8
-            cc = str( 30 + self.colors_mode8[color] )
-
-        # 256 colors mode
+            cc = ""
         else:
-            mode = 256
-            cc = str( color )
+            # 8 colors modes
+            if color in self.colors_mode8:
+                mode = 8
+                cc = str( 30 + self.colors_mode8[color] )
+
+            # 256 colors mode
+            else:
+                mode = 256
+                cc = str( color )
 
         return start + cs + self.modes[mode] + cc + "m" + text + stop
 
@@ -54,7 +63,34 @@ class _lp_smart_mark(_lp_):
         self.mark=mark
 
 
-LP_MARK = _lp_smart_mark()
+class _lp_user(_lp_):
+    ALWAYS=1
+    COLOR_ROOT="yellow"
+    COLOR_LOGGED="white"
+    COLOR_ALT="white"
 
-del _lp_smart_mark
+    def __str__(self):
+        # current user
+        cur = getpass.getuser()
+
+        # logged user
+        # WARNING: Unix only
+        log = os.getlogin()
+
+        if cur != 'root':
+            if cur != log:
+                return self.color( cur, self.COLOR_ALT )
+            else:
+                if self.ALWAYS:
+                    return self.color( cur, self.COLOR_LOGGED )
+                else:
+                    return ""
+        else:
+            return self.color( cur, self.COLOR_ROOT )
+
+
+LP_USER = _lp_user();      del _lp_user
+LP_MARK = _lp_smart_mark();del _lp_smart_mark
+
+del _lp_
 

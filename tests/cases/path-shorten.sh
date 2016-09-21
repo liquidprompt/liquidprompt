c92a09845c9b13588d70b86ee157c5ab2f5d5f14
Felix Schlitter <felixschlitter@gmail.com>
2016-09-22 05:39:35 +1200

    Add failing path shorten tests
    Interestingly enough, they only fail for zsh. So there's a difference
    between bash and zsh here. Should that difference exist?

--- tests/cases/path-shorten.sh
+++ tests/cases/path-shorten.sh
@@ -1,9 +1,22 @@
-LP_PATH_LENGTH=5
-source "$SCRIPT_DIR/tests/support/mocked-liquidprompt.sh"
-
 mkdir -p this/is/a/very/very/very/long/path
 cd       this/is/a/very/very/very/long/path
 
-eval_prompt
+assert_path () {
+    assert_ps1_plain_is "0.64 ${DETACHED_SESSION_COUNT}d [$_LP_USER_SYMBOL:$1] 2s $_LP_MARK_SYMBOL "
+}
 
-assert_ps1_plain_is "0.64 ${DETACHED_SESSION_COUNT}d [$_LP_USER_SYMBOL:/tmp … path] 2s $_LP_MARK_SYMBOL "
+echo "$PWD"
+
+for t in \
+    {0..8}:'/tmp … path' \
+    9:'/tmp … /path' \
+    10:'/tmp … g/path'
+do
+    IFS=: read -r ns p <<< "$t"
+    for n in $ns; do
+        LP_PATH_LENGTH="$n"
+        source "$SCRIPT_DIR/tests/support/mocked-liquidprompt.sh"
+        eval_prompt "LP_PATH_LENGTH=$n -> $p"
+        assert_path "$p"
+    done
+done

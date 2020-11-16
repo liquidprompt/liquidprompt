46df99503698c838ad6bb9c030a271e9fda87b15
Mark Vander Stel <mvndrstl@gmail.com>
2020-11-16 12:02:14 -0500

    Rework title to remove subshell
    Technically it only gives the option to remove a subshell. In the
    default prompt, since colors are still tied in closely to the prompt, it
    still uses the escape code stripping version, which includes a subshell
    so it can run sed.
    
    Add _lp_raw_title(), which sets the generated title without stripping
    any characters.
    
    Add lp_title(), which sets the title manually, and overrides the theme
    generated title until the user runs lp_title() without any arguments.

--- liquid.ps1
+++ liquid.ps1
@@ -43,11 +43,7 @@ LP_PS1="${LP_PS1}${LP_VCS}"
 # add return code and prompt mark
 LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
 
-# "invisible" parts
 # Get the current prompt on the fly and make it a title
-LP_TITLE="$(_lp_title "$LP_PS1")"
-
-# Insert it in the prompt
-LP_PS1="${LP_TITLE}${LP_PS1}"
+_lp_title "$LP_PS1"
 
 # vim: set et sts=4 sw=4 tw=120 ft=sh:

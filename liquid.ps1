ac3c41616e611f40cb234c02da7e29ffbc6f04bd
Mark Vander Stel <mvndrstl@gmail.com>
2020-12-09 17:00:10 -0500

    Update template example to point to documentation
    Keeping this file up to date with the default theme setup would be
    impossible, so instead of trying, make it an example only, and point to
    the documentation of the template sections which will always be kept up
    to date.
    
    Fix use of _lp_title() instead of _lp_formatted_title(). See, it is hard
    to keep this file current!

--- liquid.ps1
+++ liquid.ps1
@@ -1,32 +1,14 @@
 
 #######################################
-# LIQUID PROMPT DEFAULT TEMPLATE FILE #
+# LIQUID PROMPT EXAMPLE TEMPLATE FILE #
 #######################################
 
-# Available features:
-# LP_BATT battery
-# LP_LOAD load
-# LP_JOBS screen sessions/running jobs/suspended jobs
-# LP_USER user
-# LP_HOST hostname
-# LP_PERM a colon ":"
-# LP_PWD current working directory
-# LP_VENV Python virtual environment
-# LP_PROXY HTTP proxy
-# LP_VCS the content of the current repository
-# LP_ERR last error code
-# LP_MARK prompt mark
-# LP_TIME current time
-# LP_TTYN number of current terminal (useful in title for quick switching)
-# LP_RUNTIME runtime of last command
-# LP_MARK_PREFIX user-defined prompt mark prefix (helpful if you want 2-line prompts)
-# LP_PS1_PREFIX user-defined general-purpose prefix (default set a generic prompt as the window title)
-# LP_PS1_POSTFIX user-defined general-purpose postfix
-# LP_BRACKET_OPEN open bracket
-# LP_BRACKET_CLOSE close bracket
+# This file is not updated with new template sections. This is an example of
+# how to make a template, not what can go in one.
+# Available sections can be found in the documentation:
+# https://liquidprompt.readthedocs.io/en/stable/theme/default.html#templates
 
-# Remember that most features come with their corresponding colors,
-# see the README.
+# Remember that most features come with their corresponding colors.
 
 # add time, jobs, load and battery
 LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_TEMP}${LP_JOBS}"
@@ -36,14 +18,12 @@ LP_PS1+="${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
 LP_PS1+="${LP_PWD}${LP_BRACKET_CLOSE}${LP_SCLS}${LP_VENV}${LP_PROXY}"
 
 # Add VCS infos
-# If root, the info has not been collected unless LP_ENABLE_VCS_ROOT
-# is set.
 LP_PS1+="${LP_VCS}"
 
 # add return code and prompt mark
 LP_PS1+="${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}${LP_PS1_POSTFIX}"
 
 # Get the current prompt on the fly and make it a title
-_lp_title "$LP_PS1"
+_lp_formatted_title "$LP_PS1"
 
 # vim: set et sts=4 sw=4 tw=120 ft=sh:

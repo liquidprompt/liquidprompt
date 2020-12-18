9a00eada113cb1d5e33aa177f5b9180c25d6a843
Mark Vander Stel <mvndrstl@gmail.com>
2020-12-18 11:19:33 -0500

    Update external tool tester
    Add a check for the output being a terminal, and print help text if so.
    Add a reminder at the end of the tests to upload the file.
    Add more OS info detection.

--- tools/external-tool-tester.sh
+++ tools/external-tool-tester.sh
@@ -3,19 +3,38 @@
 # Do NOT error on failed commands
 set +e
 
+# Error if the output is a terminal
+if [ -t 1 ]; then
+  printf 'This script must be redirected to a file, or special characters will be lost
+  Ex: %s > output.txt\nOr, to upload directly:
+  %s | curl -F "sprunge=<-" "http://sprunge.us"\n' "$0" "$0"
+  exit 1
+fi
+
+# Export needed variables
+export LC_ALL=C
+
+# Print OS info
+printf -- '---Uname:\n'
+uname -a
+printf -- '---lpb_release:\n'
+lsb_release -a 2>/dev/null || printf '<none>\n'
+printf -- '---/etc/*release:\n'
+cat /etc/*release 2>/dev/null || printf '<none>\n'
+printf -- '---/etc/issue*:\n'
+cat /etc/issue* 2>/dev/null || printf '<none>\n'
+printf -- '---/proc/version:\n'
+cat /proc/version 2>/dev/null || printf '<none>\n'
+
+# Sanity check to verify special characters have not been lost on upload
+printf '\nSpecial character check: %s\n' $'\a\b\t\001\r'
+
 test_tool() {
   local stderr
   printf '\nCommand: "%s"\n--------stdout--------\n' "$*"
   { stderr="$( { "$@"; } 2>&1 1>&3 3>&- )"; } 3>&1
   printf '\n--------stderr--------\n%s\n----------------------\nReturn code: "%s"\n' "$stderr" "$?"
 }
-
-# Export needed variables
-export LC_ALL=C
-
-# Print uname
-printf 'Uname:\n'
-uname -a
 
 test_tool uname
 
@@ -62,3 +81,5 @@ test_tool acpi -t
 test_tool date '+%I %M'
 test_tool tty
 test_tool basename -- /dev/pts/0
+
+printf 'Tests complete.\nMake sure to upload the file directly, do not `cat` and copy paste!\n' >&2

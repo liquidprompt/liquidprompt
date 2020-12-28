liquidprompt/liquidprompt

commit 71321c84ab86067edca74ccb3db1b56fe830c747

Handle neagative temperature in _lp_acpi_temp

liquidprompt
@@ -1380,7 +1380,7 @@ _lp_temp_sensors()
 _lp_temp_acpi()
 {
     local i
-    for i in $(acpi -t | sed 's/.* \([0-9]*\)\.[0-9]* degrees C$/\1/p'); do
+    for i in $(acpi -t | sed 's/.* \([-]\?[0-9]*\)\.[0-9]* degrees C$/\1/p'); do
         [[ $i -gt $temperature ]] && temperature=$i
     done
 }

commit eb309422009c8f8e2a105381317b71ace5d42e13

Fix sed extended regex for acpi -t
This "bug" was introduced in 71321c84ab86067edca74ccb3db1b56fe830c747.
No one has reported it yet because any system that acpi supports
probably has a sed that allows for the '\?' notation. But our new
testing caught this on MacOS, so why not fix it.

This is a weird one though. The PR that added the support for negative
temperature did nothing to actually allow them to be reported. Since our
temp is intitialized with '0', lp_temp() will never report less than
that. I'm not sure if fixing is the right thing to do, and I don't even
know what a good starting value would be. -256?

Add the test case from the PR that first reported this issue to prevent
regression.

liquidprompt
@@ -2200,7 +2200,7 @@ __lp_temp_acpi() {
     local IFS=$' \t\n'
     # Only the integer part is retained
     for i in $(LC_ALL=C \acpi -t |
-            sed -n 's/.* \(-\?[0-9]*\)\.[0-9]* degrees C$/\1/p'); do
+            sed -n 's/.* \(-\{0,1\}[0-9]*\)\.[0-9]* degrees C$/\1/p'); do
         (( $i > ${lp_temperature:-0} )) && lp_temperature=$i
     done
 }

tests/test_acpi.sh
@@ -25,6 +25,12 @@ temp_outputs+=(
 )
 temp_values+=(23)
 
+# Unknown
+temp_outputs+=(
+"Thermal 0: ok, -267.8 degrees C"
+)
+temp_values+=(0)
+
 
 function test_acpi_battery {
 
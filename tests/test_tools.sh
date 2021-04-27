3b751856bfe701f38f842e8ae96d803a3990f13d
Mark Vander Stel <mvndrstl@gmail.com>
2021-04-27 13:22:39 -0400

    Add included themes to theme preview tests
    While (again) running the theme preview script does not really test that
    it is working, nor does it do like the old test script did and actually
    validate that output of the themes are what we expect, it will now catch
    runtime errors, which could be incredibly helpful.

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -11,6 +11,9 @@ function setUp {
 function test_theme_preview {
   # This does not really test the tool, just verify that it does not error.
   . ./tools/theme-preview.sh default
+  . ./tools/theme-preview.sh powerline ./themes/powerline/powerline.theme
+  . ./tools/theme-preview.sh powerline_full ./themes/powerline/powerline.theme
+  . ./tools/theme-preview.sh alternate_vcs ./themes/alternate_vcs/alternate_vcs.theme
 }
 
 function test_external_tool_tester {

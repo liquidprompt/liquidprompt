140902e2f7cafbd6dbe07d9e98527c82fb763e8b
Mark Vander Stel <mvndrstl@gmail.com>
2022-12-05 13:15:20 -0500

    Fix Poetry virtualenvs showing "prompt ="
    The file format for pyvenv.cfg is not very well defined, and it seems
    that the line could be any of the following:
    
    * prompt = venv
    * prompt=venv
    * prompt='venv'
    * prompt = 'venv'
    
    This gives us a lot of cases to cover, as well as making parsing the
    file very hard. The new _grep_fields() function can't be used, as it
    expects a single defined delimiter, which we don't have.
    
    Fixes #760

---
+++ tests/test_python_venv.sh
@@ -0,0 +1,45 @@
+# Error on unset variables
+set -u
+
+. ../liquidprompt --no-activate
+
+LP_ENABLE_VIRTUALENV=1
+
+test_python_venv() {
+  VIRTUAL_ENV="$SHUNIT_TMPDIR/venv"
+  mkdir "$VIRTUAL_ENV"
+
+  _lp_python_env
+  assertEquals "Python venv directory base name" venv "$lp_python_env"
+
+  echo "prompt = virtualenv" > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file basic" virtualenv "$lp_python_env"
+
+  echo "prompt = 'my venv'" > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file single quotes" "my venv" "$lp_python_env"
+
+  echo 'prompt = "second venv"' > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file double quotes" "second venv" "$lp_python_env"
+
+  echo "prompt=virtualenv" > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file basic" virtualenv "$lp_python_env"
+
+  echo "prompt='my venv'" > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file single quotes" "my venv" "$lp_python_env"
+
+  echo 'prompt="second venv"' > "$VIRTUAL_ENV/pyvenv.cfg"
+  _lp_python_env
+  assertEquals "Python venv config file double quotes" "second venv" "$lp_python_env"
+}
+
+if [ -n "${ZSH_VERSION-}" ]; then
+  SHUNIT_PARENT="$0"
+  setopt shwordsplit ksh_arrays
+fi
+
+. ./shunit2
